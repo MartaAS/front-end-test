00aef64d6ae1464cf64b3f179f29b32f96c02f76
Marta <marta.aguilar@kairosds.com>
2021-11-17 15:20:27 +0100

    feature: created search component

--- src/components/search/Search.js
+++ src/components/search/Search.js
@@ -1,8 +1,21 @@
 import React from 'react'
 
-export const Search = () => {
+export const Search = ({ setFilter }) => {
+  const handleSearch = (e) => {
+    setFilter(e.target.value)
+  }
+
   return (
-    <>
-    </>
+    <div>
+      <input
+        onKeyPress={handleSearch}
+        type='text'
+        placeholder='Busca'
+        className='form-control'
+        name='searchText'
+        onChange={handleSearch}
+        autoComplete='off'
+      />
+    </div>
   )
 }
