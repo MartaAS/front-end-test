b9b53554ec55ca578f3625330de389b23aaa45dc
Marta <marta.aguilar@kairosds.com>
2021-11-17 15:20:03 +0100

    chore: show text data

--- src/products/ProductCard.js
+++ src/products/ProductCard.js
@@ -12,10 +12,10 @@ export const ProductCard = ({
 
       <img src={imgUrl} alt={id} />
 
-      <h5 className='card-title'> {id} </h5>
-      <p className='card-text'> {model}</p>
-      <p className='card-text'> {price}</p>
-      <p className='card-text'> {brand}</p>
+      <h5 className='card-title'>id {id} </h5>
+      <p className='card-text'>model {model}</p>
+      <p className='card-text'>price {price}</p>
+      <p className='card-text'>brand {brand}</p>
       <Link to={`./product/${id}`}>
         Mas...
       </Link>
